4c72fead19237dddc47edc46335003b2d2ce47eb
Maria Scott <maria-12648430@hnc-agency.org>
2021-06-30 15:53:22 +0200

    Improvements
    * postpone events manually so they are not replayed on internal
      state changes
    * allow callback functions for handling events
    * allow shorthand forms for callback returns when the state data
      remains unchanged

--- README.md
+++ README.md
@@ -10,81 +10,90 @@ The argument given to `gen_statem:enter_wait/1` must be a map or property list o
 
 ## Options
 
-* `callback => fun((StateData0 :: term()) -> {ok, StateData1 :: term()} | {retry, StateData1 :: term()}) | {stop, StateData1 :: term()}` (mandatory).
+* `callback := fun((StateData0 :: term()) -> ok | {ok, StateData1 :: term()} | retry | {retry, StateData1 :: term()} | stop | {stop, StateData1 :: term()})` (mandatory).
   
   The callback to try and retry.
-  * If the callback returns an `ok` tuple, this is a success and the state machine transitions to the state given in the `success_state` option.
-  * If a `retry` tuple is returned, the callback is retried after the timeout defined by the `strategy` option. If the maximum number of attempts is exhausted, the state machine transitions to the state given in the `error_state` option.
-  * If a `stop` tuple is returned, this means giving up, and the state machine transitions to the state given in the `error_state` option.
-  * If the callback crashes without returning anything, the state machine transitions to the state given in the `error_state` option.
+  * If the callback returns `ok` or an `ok` tuple, this is a success and the state machine transitions to the state given in the `success_state` option.
+  * If `retry` or a `retry` tuple is returned, the callback is retried after the timeout defined by the `strategy` option. If the maximum number of attempts is exhausted, the state machine transitions to the state given in the `error_state` option.
+  * If `stop` or a `stop` tuple is returned, this means giving up, and the state machine transitions to the state given in the `error_state` option.
+  * If the callback times out or crashes without returning anything, the state machine transitions to the state given in the `error_state` option.
 
 * `callback_timeout => timeout()` (optional, default `infinity`)
 
-  Maximum allowed time for the callback to execute. If the callback does not return within this time, it is killed and the state machine transitions to the state given in the `error_state` option.
+  Maximum time for the execution of `callback`. If the callback does not return within this time, it is killed and the state machine transitions to the state given in the `error_state` option.
 
-* `strategy => {simple, StrategyOpts :: map()} | {custom, fun((Attempt :: non_neg_integer()) -> {ok, non_neg_integer()} | error)}` (mandatory)
+* `strategy := {simple, StrategyOpts :: (map() | proplists:proplist())} | {custom, fun((Attempt :: non_neg_integer()) -> {ok, Timeout :: non_neg_integer()} | error)}` (mandatory)
   
   Strategy used to calculate timeouts between retrying the function given in the `callback` option.
   
   The built-in `simple` strategy takes a map with the following options:
   
-  * `delay := non_neg_integer()` (optional, default `0`)
+  * `delay => non_neg_integer()` (optional, default `0`)
     
     Constant delay time in milliseconds.
     
-  * `time := non_neg_integer()` (optional, default `0`)
+  * `time => non_neg_integer()` (optional, default `0`)
 
     Backoff base time in milliseconds.
     
-  * `backoff := number()` (optional, default `0`)
+  * `backoff => number()` (optional, default `0`)
 
     Backoff exponent.
     
-  * `jitter := non_neg_integer()` (optional, default `0`)
+  * `jitter => non_neg_integer()` (optional, default `0`)
 
     Random value between `0` and the given value which will be added to the timeout.
     
-  * `max := non_neg_integer() | infinity` (optional, default `infinity`)
+  * `max => non_neg_integer() | infinity` (optional, default `infinity`)
   
     Maximum number of attempts.
     
-  The time between attempts is calculated as follows: Delay + Attempt<sup>Backoff</sup> * Time + random(0.0 .. 1.0) * Jitter.
+  The time between attempts is calculated as follows: delay + attempt<sup>backoff</sup> * time + random(0 .. jitter).
     
-  More advanced timeout strategies can be implemented by using the `custom` strategy, which takes a function that is given the current attempt.
+  More advanced timeout strategies can be implemented by using the `custom` strategy, which takes a function that is given the current attempt as single argument.
 
-* `success_state := State :: term()` (optional, defaults to the state in which `enter_wait/1` was called)
+* `success_state => State :: term()` (optional, defaults to the state in which `enter_wait/1` was called)
 
   State to transition to after success.
 
-* `error_state := term()` (optional, defaults to the state in which `enter_wait/1` was called)
+* `error_state => term()` (optional, defaults to the state in which `enter_wait/1` was called)
 
   State to transition to when the maximum number of attempts is exhausted.
 
-* `external_events := postpone | ignore | {reply_or_ignore, Reply :: term()}` (optional, default `postpone`)
+* `call_events => postpone | ignore | {reply, Reply :: term()} | Fun :: fun((Type :: {call, From :: gen_statem:from()}, Msg :: term(), Data0 :: term()) -> ignore | {ignore, Data1 :: term()} | postpone | {postpone, Data1 :: term()})` (optional, default `postpone`)
 
-  How to handle incoming external events (`{call, From}`, `cast` and `info`).
+  `cast_events => postpone | ignore | Fun :: fun((Type :: cast, Msg :: term(), Data0 :: term()) -> ignore | {ignore, Data1 :: term()} | postpone | {postpone, Data1 :: term()})` (optional, default `postpone`)
+
+  `info_events => postpone | ignore | Fun :: fun((Type :: info, Msg :: term(), Data0 :: term()) -> ignore | {ignore, Data1 :: term()} | postpone | {postpone, Data1 :: term()})` (optional, default `postpone`)
+
+  `timeout_events => postpone | ignore | Fun :: fun((Type :: {timeout, Name :: term()}, Msg :: term(), Data0 :: term()) -> ignore | {ignore, Data1 :: term()} | postpone | {postpone, Data1 :: term()})` (optional, default `ignore`)
+
+  How to handle any external and timeout events.
   
   * `postpone`
 
-    Postpone external events and replay them after the transition to `success_state` or `error_state`.
+    Postpone events and replay them after the transition to `success_state` or `error_state`.
     
   * `ignore`
 
-    Discard all external events.
+    Discard events.
     
+  * `{reply, Reply}` (only available for `call_events`)
+
+    Reply to call events with the given `Reply`.
+
+  * `Fun`
+
+    Call the given function with the event, message, and the current data as arguments. The function may return either `ignore` or `postpone`, resulting in the behavior described above, or `ignore` or `postpone` tuples with updated data. Note that the execution of this function has to be delayed if the `callback` is currently being executed, so consider using the static alternatives above which are always executed immediately.
+
+* `external_events => postpone | ignore | {reply_or_ignore, Reply :: term()} | {reply_or_postpone, Reply :: term()} | Fun :: fun((Type :: ({call, From :: gen_statem:from()} | cast | info), Msg :: term(), Data0 :: term()) -> ignore | {ignore, Data1 :: term()} | postpone | {postpone, Data1 :: term()})` (optional, default `postpone`)
+
+  Set handling of all external events (call, cast, info). If there exists a rule for a specific external event, it overrides this setting.
+
   * `{reply_or_ignore, Reply}`
 
-    Reply to `call` events with the given `Reply`, ignore `cast` and `info` events
+    Reply to call events with the given `Reply` and discard other external events.
 
-* `timeout_events := ignore | postpone` (optional, default `ignore`)
-  
-  How to handle generic timeout events. State and event timeouts were cancelled before.
-  
-  * `ignore`
+  * `{reply_or_postpone, Reply}`
 
-    Discard all timeout events.
-    
-  * `postpone`
-
-    Postpone timeout events and replay them after the transition to `success_state` or `error_state`.
-
+    Reply to call events with the given `Reply` and postpone other external events.
--- src/wait_statem.erl
+++ src/wait_statem.erl
@@ -9,24 +9,24 @@
 
 -record(data, {
 	opts,
-	attempt,
-	return_data
+	attempt=0,
+	return_data,
+	postponed=[]
 }).
 
-enter_wait(Opts) when is_map(Opts) ->
+enter_wait(Opts) ->
 	{
 		keep_state_and_data,
 		[
 			{push_callback_module, ?MODULE},
 			{next_event, internal, {init, Opts}}
 		]
-	};
-enter_wait(Opts) ->
-	enter_wait(proplists:to_map(Opts)).
+	}.
 
 callback_mode() ->
 	handle_event_function.
 
+-spec init(_) -> no_return().
 init(Args) ->
 	erlang:error(not_implemented, [Args]).
 
@@ -46,15 +46,17 @@ handle_event(internal, sleep, {?MODULE, undefined}, Data=#data{opts=Opts}) ->
 		error ->
 			{
 				next_state,
-				{?MODULE, returning},
+				{?MODULE, error},
 				Data,
-				[{next_event, internal, {return, error}}]
+				[{next_event, internal, return}]
 			};
 		{ok, Timeout} ->
 			{
 				next_state,
 				{?MODULE, sleeping},
-				Data#data{attempt=Data#data.attempt+1},
+				Data#data{
+					attempt=Data#data.attempt+1
+				},
 				[{state_timeout, Timeout, wakeup}]
 			}
 	end;
@@ -64,66 +66,145 @@ handle_event(state_timeout, wakeup, {?MODULE, sleeping}, Data=#data{opts=Opts}) 
 	Self=self(),
 	Tag=make_ref(),
 	{Pid, Mon}=spawn_monitor(fun () -> Self ! {Tag, Callback(Data#data.return_data)} end),
-	{next_state, {?MODULE, {executing, Tag, Pid, Mon}}, Data, [{state_timeout, CallbackTimeout, timeout}]};
-handle_event(state_timeout, timeout, {?MODULE, {executing, _, Pid, Mon}}, Data=#data{opts=Opts}) ->
-	demonitor(Mon, [flush]),
+	{
+		next_state,
+		{?MODULE, {executing, Tag, Pid, Mon}},
+		Data,
+		[{state_timeout, CallbackTimeout, timeout}]
+	};
+handle_event(state_timeout, timeout, {?MODULE, {executing, _Tag, Pid, _Mon}}, _Data) ->
 	exit(Pid, kill),
-	{next_state, maps:get(error_state, Opts), Data#data.return_data, [pop_callback_module]};
-handle_event(info, {Tag, Result}, {?MODULE, {executing, Tag, _, Mon}}, Data) ->
+	keep_state_and_data;
+handle_event(info, {Tag, Result}, {?MODULE, {executing, Tag, _Pid, Mon}}, Data) ->
 	demonitor(Mon, [flush]),
 	case Result of
+		ok ->
+			{
+				next_state,
+				{?MODULE, success},
+				Data,
+				[{next_event, internal, return}]
+			};
 		{ok, NewReturnData} ->
 			{
 				next_state,
-				{?MODULE, returning},
-				Data#data{return_data=NewReturnData},
-				[{next_event, internal, {return, success}}]
+				{?MODULE, success},
+				Data#data{
+					return_data=NewReturnData
+				},
+				[{next_event, internal, return}]
+			};
+		retry ->
+			{
+				next_state,
+				{?MODULE, undefined},
+				Data,
+				[{next_event, internal, sleep}]
 			};
 		{retry, NewReturnData} ->
 			{
 				next_state,
 				{?MODULE, undefined},
-				Data#data{return_data=NewReturnData},
+				Data#data{
+					return_data=NewReturnData
+				},
 				[{next_event, internal, sleep}]
+			};
+		stop ->
+			{
+				next_state,
+				{?MODULE, error},
+				Data,
+				[{next_event, internal, return}]
 			};
 		{stop, NewReturnData} ->
 			{
 				next_state,
-				{?MODULE, returning},
-				Data#data{return_data=NewReturnData},
-				[{next_event, internal, {return, error}}]
+				{?MODULE, error},
+				Data#data{
+					return_data=NewReturnData
+				},
+				[{next_event, internal, return}]
 			}
 	end;
-handle_event(info, {'DOWN', Mon, process, Pid, _}, {?MODULE, {executing, _, Pid, Mon}}, Data=#data{opts=Opts}) ->
-	{next_state, maps:get(error_state, Opts), Data#data.return_data, [pop_callback_module]};
-handle_event(internal, {return, success}, {?MODULE, returning}, Data=#data{opts=Opts}) ->
-	{next_state, maps:get(success_state, Opts), Data#data.return_data, [pop_callback_module]};
-handle_event(internal, {return, error}, {?MODULE, returning}, Data=#data{opts=Opts}) ->
-	{next_state, maps:get(error_state, Opts), Data#data.return_data, [pop_callback_module]};
-handle_event({call, _From}, _Msg, {?MODULE, _}, #data{opts=#{external_events:=ignore}}) ->
-	keep_state_and_data;
-handle_event({call, _From}, _Msg, {?MODULE, _}, #data{opts=#{external_events:=postpone}}) ->
-	{keep_state_and_data, [postpone]};
-handle_event({call, From}, _Msg, {?MODULE, _}, #data{opts=#{external_events:={reply_or_ignore, Reply}}}) ->
-	{keep_state_and_data, [{reply, From, Reply}]};
-handle_event(cast, _Msg, {?MODULE, _}, #data{opts=#{external_events:=ignore}}) ->
-	keep_state_and_data;
-handle_event(cast, _Msg, {?MODULE, _}, #data{opts=#{external_events:=postpone}}) ->
-	{keep_state_and_data, [postpone]};
-handle_event(cast, _Msg, {?MODULE, _}, #data{opts=#{external_events:={reply_or_ignore, _Reply}}}) ->
-	keep_state_and_data;
-handle_event(info, _Msg, {?MODULE, _}, #data{opts=#{external_events:=ignore}}) ->
-	keep_state_and_data;
-handle_event(info, _Msg, {?MODULE, _}, #data{opts=#{external_events:=postpone}}) ->
-	{keep_state_and_data, [postpone]};
-handle_event(info, _Msg, {?MODULE, _}, #data{opts=#{external_events:={reply_or_ignore, _Reply}}}) ->
-	keep_state_and_data;
-handle_event({timeout, _Name}, _Msg, {?MODULE, _}, #data{opts=#{timeout_events:=ignore}}) ->
-	keep_state_and_data;
-handle_event({timeout, _Name}, _Msg, {?MODULE, _}, #data{opts=#{timeout_events:=postpone}}) ->
-	{keep_state_and_data, [postpone]};
+handle_event(info, {'DOWN', Mon, process, Pid, _Reason}, {?MODULE, {executing, _Tag, Pid, Mon}}, Data=#data{opts=Opts}) ->
+	{
+		next_state,
+		maps:get(error_state, Opts),
+		Data#data.return_data,
+		[pop_callback_module|lists:reverse(Data#data.postponed)]
+	};
+handle_event(internal, return, {?MODULE, success}, Data=#data{opts=#{success_state:=ReturnState}}) ->
+	{
+		next_state,
+		ReturnState,
+		Data#data.return_data,
+		[pop_callback_module|lists:reverse(Data#data.postponed)]
+	};
+handle_event(internal, return, {?MODULE, error}, Data=#data{opts=#{error_state:=ReturnState}}) ->
+	{
+		next_state,
+		ReturnState,
+		Data#data.return_data,
+		[pop_callback_module|lists:reverse(Data#data.postponed)]
+	};
+handle_event(Type={call, _From}, Msg, State={?MODULE, _}, Data=#data{opts=#{call_events:=How}}) ->
+	handle_common_event(How, Type, Msg, State, Data);
+handle_event(Type=cast, Msg, State={?MODULE, _}, Data=#data{opts=#{cast_events:=How}}) ->
+	handle_common_event(How, Type, Msg, State, Data);
+handle_event(Type=info, Msg, State={?MODULE, _}, Data=#data{opts=#{info_events:=How}}) ->
+	handle_common_event(How, Type, Msg, State, Data);
+handle_event(Type={timeout, _Name}, Msg, State={?MODULE, _}, Data=#data{opts=#{timeout_events:=How}}) ->
+	handle_common_event(How, Type, Msg, State, Data);
 handle_event(_Type, _Msg, _State, _Data) ->
 	keep_state_and_data.
+
+handle_common_event(ignore, _Type, _Msg, {?MODULE, _}, _Data) ->
+	keep_state_and_data;
+handle_common_event(postpone, Type, Msg, {?MODULE, _}, Data) ->
+	{
+		keep_state,
+		Data#data{
+			postponed=[{next_event, Type, Msg}|Data#data.postponed]
+		}
+	};
+handle_common_event({reply, Reply}, {call, From}, _Msg, {?MODULE, _}, _Data) ->
+	{
+		keep_state_and_data,
+		[{reply, From, Reply}]
+	};
+handle_common_event(Fun, _Type, _Msg, {?MODULE, {executing, _, _, _}}, _Data) when is_function(Fun, 3) ->
+	{
+		keep_state_and_data,
+		[postpone]
+	};
+handle_common_event(Fun, Type, Msg, {?MODULE, _}, Data) when is_function(Fun, 3) ->
+	case Fun(Type, Msg, Data#data.return_data) of
+		ignore ->
+			keep_state_and_data;
+		{ignore, NewReturnData} ->
+			{
+				keep_state,
+				Data#data{
+					return_data=NewReturnData
+				}
+			};
+		postpone ->
+			{
+				keep_state,
+				Data#data{
+					postponed=[{next_event, Type, Msg}|Data#data.postponed]
+				}
+			};
+		{postpone, NewReturnData} ->
+			{
+				keep_state,
+				Data#data{
+					postponed=[{next_event, Type, Msg}|Data#data.postponed],
+					return_data=NewReturnData
+				}
+			}
+	end.
 
 code_change(_OldVsn, State, Data, _Extra) ->
 	{ok, State, Data}.
@@ -131,28 +212,25 @@ code_change(_OldVsn, State, Data, _Extra) ->
 get_timeout(N, {simple, #{max:=Max}}) when is_integer(Max), N>=Max ->
 	error;
 get_timeout(N, {simple, #{delay:=Delay, time:=Time, backoff:=Backoff, jitter:=Jitter}}) ->
-	{ok, calc_timeout(N, Delay, Time, Backoff, Jitter)};
+	{ok, Delay + round(calc_backoff(N, Time, Backoff) + calc_jitter(Jitter))};
 get_timeout(N, {custom, Fun}) ->
 	Fun(N).
 
-calc_timeout(0, D, _, _, 0) ->
-	D;
-calc_timeout(_, D, 0, _, 0) ->
-	D;
-calc_timeout(0, D, _, _, J) ->
-	D + round(rand:uniform() * J);
-calc_timeout(_, D, 0, _, J) ->
-	D + round(rand:uniform() * J);
-calc_timeout(_, D, T, B, 0) when B==0 ->
-	D + T;
-calc_timeout(_, D, T, B, J) when B==0 ->
-	D + T + round(rand:uniform() * J);
-calc_timeout(N, D, T, B, 0) when B==1 ->
-	D + (N * T);
-calc_timeout(N, D, T, B, J) when B==1 ->
-	D + (N * T) + round(rand:uniform() * J);
-calc_timeout(N, D, T, B, J) ->
-	D + round(math:pow(N, B) * T + rand:uniform() * J).
+calc_backoff(0, _T, _B) ->
+	0;
+calc_backoff(_N, 0, _B) ->
+	0;
+calc_backoff(_N, T, B) when B==0 ->
+	T;
+calc_backoff(N, T, B) when B==1 ->
+	N * T;
+calc_backoff(N, T, B) ->
+	math:pow(N, B) * T.
+
+calc_jitter(0) ->
+	0;
+calc_jitter(J) ->
+	rand:uniform() * J.
 
 normalize_opts(Opts, State) when is_map(Opts) ->
 	normalize_opts1(Opts, State);
@@ -164,45 +242,75 @@ normalize_opts1(Opts, _) when not is_map_key(callback, Opts) ->
 normalize_opts1(Opts, _) when not is_map_key(strategy, Opts) ->
 	error(missing_strategy);
 normalize_opts1(Opts0, State) ->
-	Defaults=#{
-		callback_timeout => infinity,
-		external_events => postpone,
-		timeout_events => ignore,
-		success_state => State,
-		error_state => State
+	Opts1=#{
+		callback => maps:get(callback, Opts0),
+		strategy => maps:get(strategy, Opts0),
+		callback_timeout => maps:get(callback_timeout, Opts0, infinity),
+		call_events => maps:get(call_events, Opts0, maps:get(external_events, Opts0, postpone)),
+		cast_events => maps:get(cast_events, Opts0, maps:get(external_events, Opts0, postpone)),
+		info_events => maps:get(info_events, Opts0, maps:get(external_events, Opts0, postpone)),
+		timeout_events => maps:get(timeout_events, Opts0, ignore),
+		success_state => maps:get(success_state, Opts0, State),
+		error_state => maps:get(error_state, Opts0, State)
 	},
-	Opts1=maps:with([strategy, callback|maps:keys(Defaults)], Opts0),
-	Opts2=maps:merge(Defaults, Opts1),
 	maps:map(
 		fun
-			(callback, V) when is_function(V) ->
-				V;
+			(callback, Callback) when is_function(Callback, 1) ->
+				Callback;
 			(strategy, {simple, StrategyOpts}) when is_map(StrategyOpts); is_list(StrategyOpts) ->
 				{simple, normalize_strategy_opts(StrategyOpts)};
-			(strategy, V={custom, Fun}) when is_function(Fun, 1) ->
-				V;
+			(strategy, Strategy={custom, Fun}) when is_function(Fun, 1) ->
+				Strategy;
 			(callback_timeout, infinity) ->
 				infinity;
-			(callback_timeout, V) when is_integer(V), V>=0 ->
-				V;
-			(external_events, postpone) ->
+			(callback_timeout, Timeout) when is_integer(Timeout), Timeout>=0 ->
+				Timeout;
+			(call_events, postpone) ->
 				postpone;
-			(external_events, ignore) ->
+			(call_events, ignore) ->
 				ignore;
-			(external_events, V={reply_or_ignore, _}) ->
-				V;
+			(call_events, CallEvents={reply, _Reply}) ->
+				CallEvents;
+			(call_events, {reply_or_postpone, Reply}) ->
+				{reply, Reply};
+			(call_events, {reply_or_ignore, Reply}) ->
+				{reply, Reply};
+			(call_events, Fun) when is_function(Fun, 3) ->
+				Fun;
+			(cast_events, postpone) ->
+				postpone;
+			(cast_events, ignore) ->
+				ignore;
+			(cast_events, {reply_or_postpone, _Reply}) ->
+				postpone;
+			(cast_events, {reply_or_ignore, _Reply}) ->
+				ignore;
+			(cast_events, Fun) when is_function(Fun, 3) ->
+				Fun;
+			(info_events, postpone) ->
+				postpone;
+			(info_events, ignore) ->
+				ignore;
+			(info_events, {reply_or_postpone, _Reply}) ->
+				postpone;
+			(info_events, {reply_or_ignore, _Reply}) ->
+				ignore;
+			(info_events, Fun) when is_function(Fun, 3) ->
+				Fun;
 			(timeout_events, ignore) ->
 				ignore;
 			(timeout_events, postpone) ->
 				postpone;
-			(success_state, V) ->
-				V;
-			(error_state, V) ->
-				V;
+			(timeout_events, Fun) when is_function(Fun, 3) ->
+				Fun;
+			(success_state, SuccessState) ->
+				SuccessState;
+			(error_state, ErrorState) ->
+				ErrorState;
 			(K, V) ->
 				error({invalid_option, {K, V}})
 		end,
-		Opts2
+		Opts1
 	).
 
 normalize_strategy_opts(Opts) when is_map(Opts) ->
@@ -211,31 +319,29 @@ normalize_strategy_opts(Opts) when is_list(Opts) ->
 	normalize_strategy_opts1(proplists:to_map(Opts)).
 
 normalize_strategy_opts1(Opts0) ->
-	Defaults=#{
-		max => infinity,
-		delay => 0,
-		time => 0,
-		backoff => 0,
-		jitter => 0
+	Opts1=#{
+		max => maps:get(max, Opts0, infinity),
+		delay => maps:get(delay, Opts0, 0),
+		time => maps:get(time, Opts0, 0),
+		backoff => maps:get(backoff, Opts0, 0),
+		jitter => maps:get(jitter, Opts0, 0)
 	},
-	Opts1=maps:with(maps:keys(Defaults), Opts0),
-	Opts2=maps:merge(Defaults, Opts1),
 	maps:map(
 		fun
 			(max, infinity) ->
 				infinity;
-			(max, V) when is_integer(V), V>=0 ->
-				V;
-			(delay, V) when is_integer(V), V>=0 ->
-				V;
-			(time, V) when is_integer(V), V>=0 ->
-				V;
-			(backoff, V) when is_number(V) ->
-				V;
-			(jitter, V) when is_integer(V) ->
-				V;
+			(max, Max) when is_integer(Max), Max>=0 ->
+				Max;
+			(delay, Delay) when is_integer(Delay), Delay>=0 ->
+				Delay;
+			(time, Time) when is_integer(Time), Time>=0 ->
+				Time;
+			(backoff, Backoff) when is_number(Backoff) ->
+				Backoff;
+			(jitter, Jitter) when is_integer(Jitter) ->
+				Jitter;
 			(K, V) ->
 				error({invalid_strategy_option, {K, V}})
 		end,
-		Opts2
+		Opts1
 	).
