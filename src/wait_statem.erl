1fe608f63bf52d9b5c1aab8a0b7195760b8e29f4
Maria Scott <maria-12648430@hnc-agency.org>
2021-07-21 16:50:34 +0200

    Refactoring

--- src/wait_statem.erl
+++ src/wait_statem.erl
@@ -1,25 +1,47 @@
 -module(wait_statem).
 -behaviour(gen_statem).
 
--export([enter_wait/1]).
+-export([enter_wait/2]).
 -export([callback_mode/0]).
 -export([init/1]).
 -export([handle_event/4]).
+-export([terminate/3]).
 -export([code_change/4]).
 
 -record(data, {
+	tag,
 	opts,
 	attempt=0,
+	last_attempt,
+	module,
+	cb_mode,
+	cur_state,
 	return_data,
 	postponed=[]
 }).
 
-enter_wait(Opts) ->
+get_callback_mode(Module) ->
+	get_callback_mode(Module:callback_mode(), undefined).
+
+get_callback_mode([Mode=state_functions|T], _) ->
+	get_callback_mode(T, Mode);
+get_callback_mode([Mode=handle_event_function|T], _) ->
+	get_callback_mode(T, Mode);
+get_callback_mode([_|T], Mode) ->
+	get_callback_mode(T, Mode);
+get_callback_mode(Mode=state_functions, _) ->
+	Mode;
+get_callback_mode(Mode=handle_event_function, _) ->
+	Mode;
+get_callback_mode(_, Mode) when Mode=/=undefined ->
+	Mode.
+
+enter_wait(Module, Opts) ->
 	{
 		keep_state_and_data,
 		[
 			{push_callback_module, ?MODULE},
-			{next_event, internal, {init, Opts}}
+			{next_event, internal, {init, Module, Opts}}
 		]
 	}.
 
@@ -30,206 +52,317 @@ callback_mode() ->
 init(Args) ->
 	erlang:error(not_implemented, [Args]).
 
-handle_event(internal, {init, Opts}, State, Data) ->
+handle_event(internal, {init, Module, Opts}, State, Data) ->
+	Tag=make_ref(),
 	{
 		next_state,
-		{?MODULE, undefined},
+		{?MODULE, awake},
 		#data{
 			opts=normalize_opts(Opts, State),
+			tag=Tag,
 			attempt=0,
+			module=Module,
+			cb_mode=get_callback_mode(Module),
+			cur_state=State,
 			return_data=Data
 		},
-		[{next_event, internal, sleep}]
+		[{next_event, internal, {Tag, sleep}}]
 	};
-handle_event(internal, sleep, {?MODULE, undefined}, Data=#data{opts=Opts}) ->
-	case get_timeout(Data#data.attempt, maps:get(strategy, Opts)) of
+handle_event(internal, {Tag, sleep}, {?MODULE, awake}, Data=#data{tag=Tag, opts=Opts}) ->
+	BaseTime=case {Data#data.last_attempt, maps:get(timing, Opts)} of
+		{undefined, _} ->
+			erlang:monotonic_time(millisecond);
+		{_, relative} ->
+			erlang:monotonic_time(millisecond);
+		{LastAttempt, absolue} ->
+			LastAttempt
+	end,
+	case get_timeout(Data#data.module, maps:get(strategy, Opts), Data#data.attempt, Data#data.cur_state, Data#data.return_data) of
 		error ->
 			{
 				next_state,
-				{?MODULE, error},
+				{?MODULE, returning},
 				Data,
-				[{next_event, internal, return}]
+				[{next_event, internal, {Tag, error}}]
 			};
-		{ok, Timeout} ->
+		{error, NewReturnData} ->
+			{
+				next_state,
+				{?MODULE, returning},
+				Data#data{
+					return_data=NewReturnData
+				},
+				[{next_event, internal, {Tag, error}}]
+			};
+		{ok, Timeout} when is_integer(Timeout), Timeout>=0 ->
 			{
 				next_state,
 				{?MODULE, sleeping},
 				Data#data{
 					attempt=Data#data.attempt+1
 				},
-				[{state_timeout, Timeout, wakeup}]
+				[{state_timeout, BaseTime+Timeout, {Tag, wakeup}, {abs, true}}]
+			};
+		{ok, Timeout, NewReturnData} when is_integer(Timeout), Timeout>=0 ->
+			{
+				next_state,
+				{?MODULE, sleeping},
+				Data#data{
+					attempt=Data#data.attempt+1,
+					return_data=NewReturnData
+				},
+				[{state_timeout, BaseTime+Timeout, {Tag, wakeup}, {abs, true}}]
 			}
 	end;
-handle_event(state_timeout, wakeup, {?MODULE, sleeping}, Data=#data{opts=Opts}) ->
-	Callback=maps:get(callback, Opts),
-	CallbackTimeout=maps:get(callback_timeout, Opts),
-	Self=self(),
-	Tag=make_ref(),
-	{Pid, Mon}=spawn_monitor(fun () -> Self ! {Tag, execute_callback(Callback, Data#data.return_data)} end),
-	{
-		next_state,
-		{?MODULE, {executing, Tag, Pid, Mon}},
-		Data,
-		[{state_timeout, CallbackTimeout, timeout}]
-	};
-handle_event(state_timeout, timeout, {?MODULE, {executing, _Tag, Pid, _Mon}}, _Data) ->
-	exit(Pid, kill),
-	keep_state_and_data;
-handle_event(info, {Tag, Result}, {?MODULE, {executing, Tag, _Pid, Mon}}, Data) ->
-	demonitor(Mon, [flush]),
-	case Result of
+handle_event(state_timeout, {Tag, wakeup}, {?MODULE, sleeping}, Data=#data{tag=Tag, opts=Opts}) ->
+	ExecTime=erlang:monotonic_time(millisecond),
+	case execute_callback(Data#data.module, maps:get(callback, Opts), Data#data.cur_state, Data#data.return_data) of
 		ok ->
 			{
 				next_state,
-				{?MODULE, success},
+				{?MODULE, returning},
 				Data,
-				[{next_event, internal, return}]
+				[{next_event, internal, {Tag, success}}]
 			};
 		{ok, NewReturnData} ->
 			{
 				next_state,
-				{?MODULE, success},
+				{?MODULE, returning},
 				Data#data{
 					return_data=NewReturnData
 				},
-				[{next_event, internal, return}]
+				[{next_event, internal, {Tag, success}}]
 			};
 		retry ->
 			{
 				next_state,
-				{?MODULE, undefined},
-				Data,
-				[{next_event, internal, sleep}]
+				{?MODULE, awake},
+				Data#data{
+					last_attempt=ExecTime
+				},
+				[{next_event, internal, {Tag, sleep}}]
 			};
 		{retry, NewReturnData} ->
 			{
 				next_state,
-				{?MODULE, undefined},
+				{?MODULE, awake},
 				Data#data{
+					last_attempt=ExecTime,
 					return_data=NewReturnData
 				},
-				[{next_event, internal, sleep}]
+				[{next_event, internal, {Tag, sleep}}]
 			};
-		stop ->
+		error ->
 			{
 				next_state,
-				{?MODULE, error},
+				{?MODULE, returning},
 				Data,
-				[{next_event, internal, return}]
+				[{next_event, internal, {Tag, error}}]
 			};
-		{stop, NewReturnData} ->
+		{error, NewReturnData} ->
 			{
 				next_state,
-				{?MODULE, error},
+				{?MODULE, returning},
 				Data#data{
 					return_data=NewReturnData
 				},
-				[{next_event, internal, return}]
+				[{next_event, internal, {Tag, error}}]
 			}
 	end;
-handle_event(info, {'DOWN', Mon, process, Pid, _Reason}, {?MODULE, {executing, _Tag, Pid, Mon}}, Data=#data{opts=Opts}) ->
+handle_event(internal, {Tag, success}, {?MODULE, returning}, Data=#data{tag=Tag, opts=#{success_state:=SuccessState}}) ->
 	{
 		next_state,
-		maps:get(error_state, Opts),
-		Data#data.return_data,
-		[pop_callback_module|lists:reverse(Data#data.postponed)]
+		Data#data.cur_state,
+		Data#data{postponed=[]},
+		lists:reverse([{next_event, internal, {Tag, return, SuccessState}}|Data#data.postponed])
 	};
-handle_event(internal, return, {?MODULE, success}, Data=#data{opts=#{success_state:=ReturnState}}) ->
+handle_event(internal, {Tag, error}, {?MODULE, returning}, Data=#data{tag=Tag, opts=#{error_state:=ErrorState}}) ->
+	{
+		next_state,
+		Data#data.cur_state,
+		Data#data{postponed=[]},
+		lists:reverse([{next_event, internal, {Tag, return, ErrorState}}|Data#data.postponed])
+	};
+handle_event(internal, {Tag, return, ReturnState}, CurState, Data=#data{tag=Tag, cur_state=CurState}) ->
 	{
 		next_state,
 		ReturnState,
 		Data#data.return_data,
-		[pop_callback_module|lists:reverse(Data#data.postponed)]
+		[pop_callback_module]
 	};
-handle_event(internal, return, {?MODULE, error}, Data=#data{opts=#{error_state:=ReturnState}}) ->
-	{
-		next_state,
-		ReturnState,
-		Data#data.return_data,
-		[pop_callback_module|lists:reverse(Data#data.postponed)]
-	};
-handle_event(Type={call, _From}, Msg, State={?MODULE, _}, Data=#data{opts=#{call_events:=How}}) ->
-	handle_common_event(How, Type, Msg, State, Data);
-handle_event(Type=cast, Msg, State={?MODULE, _}, Data=#data{opts=#{cast_events:=How}}) ->
-	handle_common_event(How, Type, Msg, State, Data);
-handle_event(Type=info, Msg, State={?MODULE, _}, Data=#data{opts=#{info_events:=How}}) ->
-	handle_common_event(How, Type, Msg, State, Data);
-handle_event(Type={timeout, _Name}, Msg, State={?MODULE, _}, Data=#data{opts=#{timeout_events:=How}}) ->
-	handle_common_event(How, Type, Msg, State, Data);
-handle_event(_Type, _Msg, _State, _Data) ->
-	keep_state_and_data.
-
-handle_common_event(ignore, _Type, _Msg, {?MODULE, _}, _Data) ->
-	keep_state_and_data;
-handle_common_event(postpone, Type, Msg, {?MODULE, _}, Data) ->
-	{
-		keep_state,
-		Data#data{
-			postponed=[{next_event, Type, Msg}|Data#data.postponed]
-		}
-	};
-handle_common_event({reply, Reply}, {call, From}, _Msg, {?MODULE, _}, _Data) ->
-	{
-		keep_state_and_data,
-		[{reply, From, Reply}]
-	};
-handle_common_event(_Callback, _Type, _Msg, {?MODULE, {executing, _, _, _}}, _Data) ->
+handle_event(_Event, _Msg, CurState, #data{cur_state=CurState}) ->
 	{
 		keep_state_and_data,
 		[postpone]
 	};
-handle_common_event(Callback, Type, Msg, {?MODULE, _}, Data) ->
-	case execute_event(Callback, Type, Msg, Data#data.return_data) of
-		ignore ->
+handle_event(Type, Msg, State={?MODULE, _}, Data) ->
+	handle_common_event(Type, Msg, State, Data).
+
+handle_common_event(Type, Msg, {?MODULE, _}, Data) ->
+	case execute_event(Data#data.cb_mode, Data#data.module, Type, Msg, Data#data.cur_state, Data#data.return_data) of
+		keep_state_and_data ->
 			keep_state_and_data;
-		{ignore, NewReturnData} ->
+		{keep_state_and_data, Actions} ->
+			case validate_actions(Actions) of
+				{true, Actions1} ->
+					{
+						keep_state,
+						Data#data{
+							postponed=[{next_event, Type, Msg}|Data#data.postponed]
+						},
+						Actions1
+					};
+				{false, Actions1} ->
+					{
+						keep_state_and_data,
+						Actions1
+					}
+			end;
+		{keep_state, NewReturnData} ->
 			{
 				keep_state,
 				Data#data{
 					return_data=NewReturnData
 				}
 			};
-		postpone ->
+		{keep_state, NewReturnData, Actions} ->
+			case validate_actions(Actions) of
+				{true, Actions1} ->
+					{
+						keep_state,
+						Data#data{
+							return_data=NewReturnData,
+							postponed=[{next_event, Type, Msg}|Data#data.postponed]
+						},
+						Actions1
+					};
+				{false, Actions1} ->
+					{
+						keep_state,
+						Data#data{
+							return_data=NewReturnData
+						},
+						Actions1
+					}
+			end;
+		{next_state, NewState, NewReturnData} when NewState=:=Data#data.cur_state ->
 			{
 				keep_state,
 				Data#data{
-					postponed=[{next_event, Type, Msg}|Data#data.postponed]
+					return_data=NewReturnData
 				}
 			};
-		{postpone, NewReturnData} ->
+		{next_state, NewState, NewReturnData, Actions} when NewState=:=Data#data.cur_state ->
+			case validate_actions(Actions) of
+				{true, Actions1} ->
+					{
+						keep_state,
+						Data#data{
+							return_data=NewReturnData,
+							postponed=[{next_event, Type, Msg}|Data#data.postponed]
+						},
+						Actions1
+					};
+				{false, Actions1} ->
+					{
+						keep_state,
+						Data#data{
+							return_data=NewReturnData
+						},
+						Actions1
+					}
+			end;
+		stop ->
+			stop;
+		{stop, Reason} ->
 			{
-				keep_state,
+				stop,
+				Reason
+			};
+		{stop, Reason, NewReturnData} ->
+			{
+				stop,
+				Reason,
 				Data#data{
-					postponed=[{next_event, Type, Msg}|Data#data.postponed],
+					return_data=NewReturnData
+				}
+			};
+		{stop_and_reply, Reason, Replies} ->
+			{
+				stop_and_reply,
+				Reason,
+				Replies
+			};
+		{stop_and_reply, Reason, Replies, NewReturnData} ->
+			{
+				stop_and_reply,
+				Reason,
+				Replies,
+				Data#data{
 					return_data=NewReturnData
 				}
 			}
 	end.
 
+terminate(Reason, _State, Data=#data{module=Module}) ->
+	Module:terminate(Reason, Data#data.cur_state, Data#data.return_data).
+
 code_change(_OldVsn, State, Data, _Extra) ->
 	{ok, State, Data}.
 
-execute_callback({Mod, Fun}, Data) ->
-	Mod:Fun(Data);
-execute_callback(Fun, Data) ->
-	Fun(Data).
+validate_actions(Actions0) when is_list(Actions0) ->
+	{Postpone, Actions1}=lists:foldl(
+		fun
+			(postpone, {_, Acc}) ->
+				{true, Acc};
+			({postpone, Pp}, {_, Acc}) when is_boolean(Pp) ->
+				{Pp, Acc};
+			(Action, {PpAcc, Acc}) ->
+				true=valid_action(Action),
+				{PpAcc, [Action|Acc]}
+		end,
+		{false, []},
+		Actions0
+	),
+	{Postpone, lists:reverse(Actions1)};
+validate_actions(Action) ->
+	validate_actions([Action]).
 
-execute_custom_timeout({Mod, Fun}, N) ->
-	Mod:Fun(N);
-execute_custom_timeout(Fun, N) ->
-	Fun(N).
+valid_action({next_event, _Event, _Content}) ->
+	true;
+valid_action({reply, _From, _Reply}) ->
+	true;
+valid_action(hibernate) ->
+	true;
+valid_action({hibernate, _Hibernate}) ->
+	true;
+valid_action({{timeout, _Name}, _TimeoutiOrUpdate, _Content}) ->
+	true;
+valid_action({{timeout, _Name}, _Timeout, _Content, _Options}) ->
+	true;
+valid_action({{timeout, _Name}, cancel}) ->
+	true;
+valid_action(_Action) ->
+	false.
 
-execute_event({Mod, Fun}, Type, Msg, Data) ->
-	Mod:Fun(Type, Msg, Data);
-execute_event(Fun, Type, Msg, Data) ->
-	Fun(Type, Msg, Data).
+execute_callback(Module, Fun, State, Data) when is_atom(Fun) ->
+	Module:Fun(State, Data);
+execute_callback(_Module, Fun, State, Data) when is_function(Fun, 2) ->
+	Fun(State, Data).
 
-get_timeout(N, {simple, #{max:=Max}}) when is_integer(Max), N>=Max ->
+execute_event(state_functions, Module, Event, Msg, State, Data) ->
+	Module:State(Event, Msg, Data);
+execute_event(handle_event_function, Module, Event, Msg, State, Data) ->
+	Module:handle_event(Event, Msg, State, Data).
+
+get_timeout(_Module, {simple, #{max:=Max}}, N, _State, _Data) when is_integer(Max), N>=Max ->
 	error;
-get_timeout(N, {simple, #{delay:=Delay, time:=Time, backoff:=Backoff, jitter:=Jitter}}) ->
-	{ok, Delay + round(calc_backoff(N, Time, Backoff) + calc_jitter(Jitter))};
-get_timeout(N, {custom, Callback}) ->
-	execute_custom_timeout(Callback, N).
+get_timeout(_Module, {simple, #{delay:=Delay, time:=Time, backoff:=Backoff, jitter:=Jitter}}, N, _State, _Data) ->
+	{ok, max(0, Delay + round(calc_backoff(N, Time, Backoff) + calc_jitter(Jitter)))};
+get_timeout(Module, {custom, Fun}, N, State, Data) when is_atom(Fun) ->
+	Module:Fun(N, State, Data);
+get_timeout(_Module, {custom, Fun}, N, State, Data) when is_function(Fun, 3) ->
+	Fun(N, State, Data).
 
 calc_backoff(0, _T, _B) ->
 	0;
@@ -260,76 +393,26 @@ normalize_opts1(Opts0, State) ->
 	Opts1=#{
 		callback => maps:get(callback, Opts0),
 		strategy => maps:get(strategy, Opts0),
-		callback_timeout => maps:get(callback_timeout, Opts0, infinity),
-		call_events => maps:get(call_events, Opts0, maps:get(external_events, Opts0, postpone)),
-		cast_events => maps:get(cast_events, Opts0, maps:get(external_events, Opts0, postpone)),
-		info_events => maps:get(info_events, Opts0, maps:get(external_events, Opts0, postpone)),
-		timeout_events => maps:get(timeout_events, Opts0, ignore),
+		timing => maps:get(timing, Opts0, relative),
 		success_state => maps:get(success_state, Opts0, State),
 		error_state => maps:get(error_state, Opts0, State)
 	},
 	maps:map(
 		fun
-			(callback, Callback={Mod, Fun}) when is_atom(Mod), is_atom(Fun) ->
-				Callback;
-			(callback, Fun) when is_function(Fun, 1) ->
+			(callback, Fun) when is_atom(Fun) ->
+				Fun;
+			(callback, Fun) when is_function(Fun, 3) ->
 				Fun;
 			(strategy, {simple, StrategyOpts}) when is_map(StrategyOpts); is_list(StrategyOpts) ->
 				{simple, normalize_strategy_opts(StrategyOpts)};
-			(strategy, Strategy={custom, {Mod, Fun}}) when is_atom(Mod), is_atom(Fun) ->
+			(strategy, Strategy={custom, Fun}) when is_atom(Fun) ->
 				Strategy;
-			(strategy, Strategy={custom, Fun}) when is_function(Fun, 1) ->
+			(strategy, Strategy={custom, Fun}) when is_function(Fun, 3) ->
 				Strategy;
-			(callback_timeout, infinity) ->
-				infinity;
-			(callback_timeout, Timeout) when is_integer(Timeout), Timeout>=0 ->
-				Timeout;
-			(call_events, postpone) ->
-				postpone;
-			(call_events, ignore) ->
-				ignore;
-			(call_events, CallEvents={reply, _Reply}) ->
-				CallEvents;
-			(call_events, {reply_or_postpone, Reply}) ->
-				{reply, Reply};
-			(call_events, {reply_or_ignore, Reply}) ->
-				{reply, Reply};
-			(call_events, Callback={Mod, Fun}) when is_atom(Mod), is_atom(Fun) ->
-				Callback;
-			(call_events, Fun) when is_function(Fun, 3) ->
-				Fun;
-			(cast_events, postpone) ->
-				postpone;
-			(cast_events, ignore) ->
-				ignore;
-			(cast_events, {reply_or_postpone, _Reply}) ->
-				postpone;
-			(cast_events, {reply_or_ignore, _Reply}) ->
-				ignore;
-			(cast_events, Callback={Mod, Fun}) when is_atom(Mod), is_atom(Fun) ->
-				Callback;
-			(cast_events, Fun) when is_function(Fun, 3) ->
-				Fun;
-			(info_events, postpone) ->
-				postpone;
-			(info_events, ignore) ->
-				ignore;
-			(info_events, {reply_or_postpone, _Reply}) ->
-				postpone;
-			(info_events, {reply_or_ignore, _Reply}) ->
-				ignore;
-			(info_events, Callback={Mod, Fun}) when is_atom(Mod), is_atom(Fun) ->
-				Callback;
-			(info_events, Fun) when is_function(Fun, 3) ->
-				Fun;
-			(timeout_events, ignore) ->
-				ignore;
-			(timeout_events, postpone) ->
-				postpone;
-			(timeout_events, Callback={Mod, Fun}) when is_atom(Mod), is_atom(Fun) ->
-				Callback;
-			(timeout_events, Fun) when is_function(Fun, 3) ->
-				Fun;
+			(timing, Timing=relative) ->
+				Timing;
+			(timing, Timing=absolute) ->
+				Timing;
 			(success_state, SuccessState) ->
 				SuccessState;
 			(error_state, ErrorState) ->
